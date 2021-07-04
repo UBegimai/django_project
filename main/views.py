bf9ed6481d24a4a749dbda5cfc66b41cfe0c476f
UBegimai <umeibegimai@gmail.com>
2021-07-04 21:23:32 +0600

    filter

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django_filters import rest_framework as filters
 from rest_framework.filters import OrderingFilter
-from rest_framework import viewsets
-
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from .filters import ProductFilter
-from .models import Product
+from .models import Product, Review
+from .permissions import IsAuthorOrAdminPermission
 
-from .serializers import ProductListSerializer, ProductDetailsSerializer
+from .serializers import ProductListSerializer, ProductDetailsSerializer, ReviewSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -20,3 +21,22 @@ class ProductViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return ProductListSerializer
         return self.serializer_class
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        elif self.action in ['create_review', 'like']:
+            return [IsAuthenticated()]
+        return []
+
+class ReviewViewSet(mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        return [IsAuthorOrAdminPermission()]
